e85063567a757c355dbae3ec07f5f60b5eca3ad2
nagesh rathod <rathodnagesh0005@gmail.com>
2023-04-05 17:12:20 +0530

    Updated config file

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 CSV_FILE_PATH = r"medical_insurance.csv"
 
-MODEL_FILE_PATH = r"artifacts\\model.pkl"
+MODEL_FILE_PATH = r"artifacts//model.pkl"
 
-SCALER_FILE_PATH = r"artifacts\\scaler.pkl"
+SCALER_FILE_PATH = r"artifacts//scaler.pkl"
 
-MODEL_DATA_FILE = r"artifacts\\model_data.json"
+MODEL_DATA_FILE = r"artifacts//model_data.json"
 
 PORT = 5008
